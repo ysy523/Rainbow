8c305221d91cedbfe1cec124f85b153d9d6f26ea
ysy523 <shanyu523@gmail.com>
2020-10-07 11:24:21 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'SEP 11, 2020',
+  birthdate: 'OCT 11, 2020',
   name: 'Rainbow❤️'
 };
 
